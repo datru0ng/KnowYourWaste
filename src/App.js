10bd454f67997224bf30b5826621d05b3e28bd02
datru0ng <datru0ng@uw.edu>
2022-02-24 10:44:05 -0800

    Got the camera to kinda fit in the middle of the screen

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import {GiMagnifyingGlass} from 'react-icons/gi';
 import {AiFillCamera} from 'react-icons/ai';
 import {GoGraph} from 'react-icons/go';
 import {CgProfile} from 'react-icons/cg';
+import {useState} from "react";
+import Camera from 'react-snap-pic'
 
 function App() {
+  //const [showCamera, setShowCamera] = useState(false)
+
+  let takePicture = (img) => {
+    console.log(img)
+    //setShowCamera(false)
+  }
+
   return (
     <div className="App">
       <header className='Header'>
         <span className='Title'>KnowYourWaste</span>
       </header>
+
       <div className='Middle'>
-        <button className='Camera'>
-          <AiFillCamera size="35"/>
-        </button>
+        <Camera takePicture={takePicture}/>
       </div>
+
       <div className='MenuButtons'>
         <GiMagnifyingGlass size="24"/>
         <AiFillCamera size="24"/>
